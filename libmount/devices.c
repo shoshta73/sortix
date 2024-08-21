89c6f81045cd8a246f5f2928bd0541db47a66818
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2024-08-21 16:27:13 +0200

    Fix libmount devices_iterate_open_callback error handling.

--- libmount/devices.c
+++ libmount/devices.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Jonas 'Sortie' Termansen.
+ * Copyright (c) 2015, 2024 Jonas 'Sortie' Termansen.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -100,7 +100,7 @@ static bool devices_iterate_open_callback(void* ctx_ptr, const char* path)
 	{
 		int true_errno = errno;
 		struct stat st;
-		if ( lstat(path, 0) == 0 && !S_ISBLK(st.st_mode) )
+		if ( lstat(path, &st) == 0 && !S_ISBLK(st.st_mode) )
 			return true;
 		errno = true_errno;
 		return false;
