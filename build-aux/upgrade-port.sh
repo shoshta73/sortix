ee0e6506a9d87efe07d6b0604b12a346e31f07c3
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2024-09-05 21:19:08 +0200

    Add port(5) UPGRADE_BLOCKED for ports that cannot be upgraded at the moment.
    Fix detecting new releases on github by using the json API.
    
    Fix detecting versions older than the current version.
    
    Use wget --spider to speed up the download check.

--- build-aux/upgrade-port.sh
+++ build-aux/upgrade-port.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 set -e
 
-NAME=$(tix-vars -d '' "$1" NAME)
+if [ -t 1 ]; then
+  RED='\033[91m'
+  YELLOW='\033[93m'
+  GREEN='\033[92m'
+  RESET='\033[m'
+else
+  RED=''
+  YELLOW=''
+  GREEN=''
+  RESET=''
+fi
+
 COMPRESSION=$(tix-vars -d '' "$1" COMPRESSION)
 DISTNAME_REGEX=$(tix-vars -d '' "$1" DISTNAME_REGEX)
+NAME=$(tix-vars -d '' "$1" NAME)
 RELEASE_SEARCH_PAGE=$(tix-vars -d '' "$1" RELEASE_SEARCH_PAGE)
 RELEASE_SEARCH_REGEX=$(tix-vars -d '' "$1" RELEASE_SEARCH_REGEX)
+UPGRADE_BLOCKED=$(tix-vars -d '' "$1" UPGRADE_BLOCKED)
 UPSTREAM_ARCHIVE=$(tix-vars -d '' "$1" UPSTREAM_ARCHIVE)
 UPSTREAM_SITE=$(tix-vars -d '' "$1" UPSTREAM_SITE)
 VERSION=$(tix-vars -d '' "$1" VERSION)
@@ -22,7 +35,7 @@ TAR_REGEX="(\.tar(\.(gz|bz2|xz)))"
 if [ -z "$RELEASE_SEARCH_PAGE" ]; then
   case "$UPSTREAM_SITE" in
   https://github.com/*/releases/*)
-    RELEASE_SEARCH_PAGE=$(echo "$UPSTREAM_SITE" | grep -Eo '.*/releases/');;
+    RELEASE_SEARCH_PAGE=$(echo "$UPSTREAM_SITE" | sed -E -e 's,https://github.com/(.*)/releases/.*,https://api.github.com/repos/\1/releases,');;
   *) RELEASE_SEARCH_PAGE="$UPSTREAM_SITE/";;
   esac
 fi
@@ -54,7 +67,7 @@ instantiate() {
   upgrade_version "$1" "$2" | tix-vars - "$3"
 }
 
-LATEST=$(curl -Ls "$RELEASE_SEARCH_PAGE" | grep -Eo "$RELEASE_SEARCH_REGEX" | sort -Vu | tail -1)
+LATEST=$(curl --compressed -Ls "$RELEASE_SEARCH_PAGE" | grep -Eo "$RELEASE_SEARCH_REGEX" | sort -Vu | tail -1)
 case "$LATEST" in
 *.tar) COMPRESSION=tar;;
 *.tar.gz) COMPRESSION=tar.gz;;
@@ -63,31 +76,28 @@ case "$LATEST" in
 esac
 NEW_VERSION="$(echo "$LATEST" | sed -E "s,$RELEASE_SEARCH_REGEX,\1,")"
 
-if [ -t 1 ]; then
-  RED='\033[91m'
-  GREEN='\033[92m'
-  RESET='\033[m'
-else
-  RED=''
-  GREEN=''
-  RESET=''
-fi
-
 if [ -z "$NEW_VERSION" ]; then
   printf "$RED%s$RESET\n" "$UPSTREAM_ARCHIVE failed to find available versions: $RELEASE_SEARCH_PAGE | grep -E '$RELEASE_SEARCH_REGEX'"
   exit 1
 fi
 
+NEW_VERSION=$({ echo "$NEW_VERSION" && echo "$VERSION"; } | sort -Vu | tail -1)
+
 NEW_UPSTREAM_SITE=$(instantiate "$1" "$NEW_VERSION" "UPSTREAM_SITE")
 NEW_UPSTREAM_ARCHIVE=$(instantiate "$1" "$NEW_VERSION" "UPSTREAM_ARCHIVE")
 
-if ! wget -q "$NEW_UPSTREAM_SITE/$NEW_UPSTREAM_ARCHIVE" -O /dev/null; then
+if ! wget -q --spider "$NEW_UPSTREAM_SITE/$NEW_UPSTREAM_ARCHIVE"; then
   printf "$RED%s$RESET\n" "$NAME failed to download: $NEW_UPSTREAM_SITE/$NEW_UPSTREAM_ARCHIVE"
   exit 1
 fi
 
 if [ "$VERSION" = "$NEW_VERSION" ]; then
   echo "$NAME $VERSION is up to date"
+  exit
+fi
+
+if [ -n "$UPGRADE_BLOCKED" ]; then
+  printf "$YELLOW%s$RESET\n" "$NAME $VERSION is blocked ($UPGRADE_BLOCKED) -> $NEW_VERSION $NEW_UPSTREAM_SITE/$NEW_UPSTREAM_ARCHIVE"
   exit
 fi
 
