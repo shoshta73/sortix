546d11e0b83eda3193a22c3bb74fcdfb1e993a47
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2024-10-07 13:11:14 +0200

    Add sysconf(3) _SC_IOV_MAX, _SC_NPROCESSORS_CONF, and _SC_NPROCESSORS_ONLN.

--- libc/unistd/sysconf.c
+++ libc/unistd/sysconf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2014 Jonas 'Sortie' Termansen.
+ * Copyright (c) 2013, 2014, 2024 Jonas 'Sortie' Termansen.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -18,22 +18,27 @@
  */
 
 #include <errno.h>
-#include <stdio.h>
+#include <limits.h>
 #include <signal.h>
+#include <stdio.h>
 #include <unistd.h>
+
+#include <sortix/limits.h>
 
 long sysconf(int name)
 {
 	switch ( name )
 	{
 	case _SC_CLK_TCK: return 1000;
-	case _SC_PAGESIZE: case _SC_PAGE_SIZE:
-		return getpagesize();
-	case _SC_OPEN_MAX: return 0x10000;
-	case _SC_RTSIG_MAX: return (SIGRTMAX+1) - SIGRTMIN;
 	case _SC_GETGR_R_SIZE_MAX: return -1;
 	case _SC_GETPW_R_SIZE_MAX: return -1;
+	case _SC_IOV_MAX: return IOV_MAX;
 	case _SC_MONOTONIC_CLOCK: return _POSIX_MONOTONIC_CLOCK;
+	case _SC_NPROCESSORS_CONF: return 1;
+	case _SC_NPROCESSORS_ONLN: return 1;
+	case _SC_OPEN_MAX: return 0x10000;
+	case _SC_PAGESIZE: case _SC_PAGE_SIZE: return getpagesize();
+	case _SC_RTSIG_MAX: return (SIGRTMAX+1) - SIGRTMIN;
 	default:
 		fprintf(stderr, "%s:%u warning: %s(%i) is unsupported\n",
 		        __FILE__, __LINE__, __func__, name);
