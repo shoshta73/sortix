33be15d58b692203c59f9e1afe25a14bc1ef7cf1
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2025-10-08 01:52:28 +0200

    Fix pthread_rwlockattr_{init,destroy}(3) missing from libc Makefile.

--- libc/pthread/pthread_rwlockattr_destroy.c
+++ libc/pthread/pthread_rwlockattr_destroy.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Jonas 'Sortie' Termansen.
+ * Copyright (c) 2013, 2025 Jonas 'Sortie' Termansen.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -21,5 +21,6 @@
 
 int pthread_rwlockattr_destroy(pthread_rwlockattr_t* attr)
 {
+	(void) attr;
 	return 0;
 }
