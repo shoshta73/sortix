7207d6cb3eba57eef56a961acd2fbe265ad36dc7
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2025-11-09 22:17:51 +0100

    Fix <sys/statvfs.h> namespace pollution.

--- libc/include/sys/statvfs.h
+++ libc/include/sys/statvfs.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Jonas 'Sortie' Termansen.
+ * Copyright (c) 2014, 2025 Jonas 'Sortie' Termansen.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -31,7 +31,9 @@ extern "C" {
 #endif
 
 int fstatvfs(int, struct statvfs*);
+#if __USE_SORTIX
 int fstatvfsat(int, const char* __restrict, struct statvfs* __restrict, int);
+#endif
 int statvfs(const char* __restrict, struct statvfs* __restrict);
 
 #ifdef __cplusplus
