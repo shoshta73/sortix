ade1fc4b6fcf74700cc6c5df677171b428efa4c1
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2026-02-06 20:18:15 +0100

    Fix iswctype not handling (wctype_t) 0.
    POSIX says it always returns 0 in this case.

--- libc/wctype/iswctype.c
+++ libc/wctype/iswctype.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2012 Jonas 'Sortie' Termansen.
+ * Copyright (c) 2011, 2012, 2026 Jonas 'Sortie' Termansen.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -21,5 +21,5 @@
 
 int iswctype(wint_t c, wctype_t wct)
 {
-	return wct(c);
+	return wct ? wct(c) : 0;
 }
